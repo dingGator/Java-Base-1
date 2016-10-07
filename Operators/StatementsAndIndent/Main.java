30ca86cc8f7a7265016f5427c6f3ad22c3a075ea
dingGator <kim@chucrew.net>
2016-10-07 06:42:39 -0400

    Statements_Indents
    Statements and Indents Exercise from Udemy

---
+++ Operators/StatementsAndIndent/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int myVariable = 50;  // java statement int...50;
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a Test");
+        System.out.println("This is"
+                + " another "
+                + " still more ");
+        
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.println("This is");
+        
+    }
+    
+}
