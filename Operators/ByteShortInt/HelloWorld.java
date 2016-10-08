d90a27db1dc3f3e1436436da8aded2f8a0bb5095
dingGator <kim@chucrew.net>
2016-10-08 08:34:04 -0400

    Hello_World_Exercise
    Hello World Exercise from Udemy class

--- Operators/ByteShortInt/HelloWorld.java
+++ Operators/ByteShortInt/HelloWorld.java
@@ -2,14 +2,14 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
 
-package helloworld;
+ package helloworld;
 
-/**
+ /**
  *
- * @author Ki
+ * @author KC
  */
+
 public class HelloWorld {
 
     /**
@@ -17,17 +17,17 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello Kim Chu!");
-        
-        int myFirstNumber = (10+5)+(2*10);
-        int mySecondNumber =12;
-        int myThirdNumber =myFirstNumber *2;
-        int myTotal =(myFirstNumber)+mySecondNumber +myThirdNumber;
-        
-        int myLastOne = 1000-myTotal;
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = (myFirstNumber) + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
         System.out.println(myFirstNumber);
-        System.out.println("mySecondNumber");  
-          System.out.println(myTotal);
-            System.out.println(myLastOne);
+        System.out.println("mySecondNumber");
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
     }
-    
+
 }
