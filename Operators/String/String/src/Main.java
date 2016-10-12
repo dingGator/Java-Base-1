25a218fdacaf1025fd5b24727df3f132df2fcc64
dingGator <kim@chucrew.net>
2016-10-12 12:17:31 -0400

    String_Exercise
    String Exercise from Udemy class

--- Operators/String/String/src/Main.java
+++ Operators/String/String/src/Main.java
@@ -24,28 +24,22 @@ public class Main {
         //boolean
         //string  not primitive type
         String myString = "This is a string";
-        System.out.println("myString is = "+ myString);
-        myString = myString +",and this is more.";
-        System.out.println("myString is = "+ myString);
-        myString = myString +" \u00A9 2015";
-        System.out.println("myString is = "+ myString);
+        System.out.println("myString is = " + myString);
+        myString = myString + ",and this is more.";
+        System.out.println("myString is = " + myString);
+        myString = myString + " \u00A9 2015";
+        System.out.println("myString is = " + myString);
         String numberString = "250.55";
-        numberString = numberString +"49.55";
-        System.out.println("The result is  "+ numberString);
-        String lastString ="10";
-        int myInt =50;
+        numberString = numberString + "49.55";
+        System.out.println("The result is  " + numberString);
+        String lastString = "10";
+        int myInt = 50;
         lastString = lastString + myInt;
-        System.out.println("lastString is = "+ lastString);
+        System.out.println("lastString is = " + lastString);
         double doubleNumber = 120.47;
-        lastString=lastString+doubleNumber;
-        System.out.println("lastString is = "+ lastString);
-        
-        
-        
-        
-             
-        
-        
+        lastString = lastString + doubleNumber;
+        System.out.println("lastString is = " + lastString);
+
     }
-    
+
 }
