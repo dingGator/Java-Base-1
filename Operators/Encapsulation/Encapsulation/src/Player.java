f853d6402fab21249bab7124a8bd962fc7e00b64
dingGator <kim@chucrew.net>
2016-10-14 18:44:01 -0400

    Encapsulation_Exercise
    Encapsulation Exercise from Udemy class

---
+++ Operators/Encapsulation/Encapsulation/src/Player.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Player {
+    public String fullname;
+    public int health;
+    public String weapon;
+    public void loseHealth(int damage){
+        this.health = this.health -damage;
+        if (this.health<=0){
+            System.out.println("Player knocked out");
+        }
+    }
+    public int healthRemaining(){
+        return this.health;
+    }
+}
