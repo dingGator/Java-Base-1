dac62a3bbe8ec72ac6467549ea3641e0eed2f49e
dingGator <kim@chucrew.net>
2016-10-15 09:35:01 -0400

    Keywords_And_Expressions
    Keywords And Expressions from Udemy class

--- Operators/KeywordsAndExpressions/KeywordsAndExpressions/src/Main.java
+++ Operators/KeywordsAndExpressions/KeywordsAndExpressions/src/Main.java
@@ -14,26 +14,22 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int int2=5;
+        int int2 = 5;
         //false, true.null
         //a mile is 1.609344 kilometres
         double kilometres = (100 * 1.609344);
         int highScore = 50;
-        if (highScore == 50)
-          System.out.println("This is an expression");
-       
-        //Which part is an expression
-        int score =100;  //expr  is: score=100
-        
-        if (score>99){   //expr   is: in ()  
-          System.out.println("Which is an expression");//expr is: "which is ..."
-          score = 0;//expr is: score =0
+        if (highScore == 50) {
+            System.out.println("This is an expression");
         }
-        
-        
-          
-        
-        
-    }}
-    
 
+        //Which part is an expression
+        int score = 100;  //expr  is: score=100
+
+        if (score > 99) {   //expr   is: in ()  
+            System.out.println("Which is an expression");//expr is: "which is ..."
+            score = 0;//expr is: score =0
+        }
+
+    }
+}
