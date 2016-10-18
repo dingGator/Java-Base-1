94d548856b5f8aa08eef9482ce6a67dfe0e0b7b3
dingGator <kim@chucrew.net>
2016-10-18 08:15:47 -0400

    ByteShortInt_Exercise
    ByteShortInt Exercise from Udemy class

--- Operators/ByteShortInt/ByteShortInt/src/byteshortint/ByteShortInt.java
+++ Operators/ByteShortInt/ByteShortInt/src/byteshortint/ByteShortInt.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package byteshortint;
 
 /**
@@ -16,28 +15,26 @@ public class ByteShortInt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
- 
+
         //int has a width of 32
-        int myMinValue =-2_147_483_648;
+        int myMinValue = -2_147_483_648;
         int myMaxValue = 2_147_483_647;
-        int myTotal = (myMinValue/2);
-          System.out.println("myTotal = "+myTotal);
-        
+        int myTotal = (myMinValue / 2);
+        System.out.println("myTotal = " + myTotal);
+
         //byte has a width of 8
-        byte myByteValue =-128;
-        byte myNewByteValue =(byte)(myByteValue/2);
-          System.out.println("myNewByteValue = "+myNewByteValue);
-        
-        
+        byte myByteValue = -128;
+        byte myNewByteValue = (byte) (myByteValue / 2);
+        System.out.println("myNewByteValue = " + myNewByteValue);
+
         //short has a width of 16
         short myShortValue = 32767;
-        short myNewShortValue = (short)(myShortValue/2);
-        
+        short myNewShortValue = (short) (myShortValue / 2);
+
         //long has a width of 64
         long myLongValue = 9_223_370_036_854_775_807L;
-        
-        
+
 // TODO code application logic here
     }
-    
+
 }
