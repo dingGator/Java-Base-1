5a763c69a96a3788bfc969b0b2523d61fcc5fcc6
dingGator <kim@chucrew.net>
2016-10-19 15:21:03 -0400

    Method_Overloading_Exercise
    Method Overloading Exercise from Udemy class

--- Method_Overloading/MethodOverloading/src/Main.java
+++ Method_Overloading/MethodOverloading/src/Main.java
@@ -15,11 +15,11 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int newScore = calculateScore("Tim",500);
+        int newScore = calculateScore("Tim", 500);
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
-        
+
         // create a mehod calcFeetAndInchesToCentimeters
         // two paramenters:  feet , inches
         // validate feet paramenter >=0
@@ -41,99 +41,85 @@ public class Main {
         // it can calculate correctly
         // use double for datatypes 
         // 1 inch = 2.54cm  and one foot = 12 inches
-        double inches =300;
-        double conversionInchesToCentimeters=calcFeetAndInchesToCentimeters(inches);
-        System.out.println("  "+ inches+ " inches is  " +
-                conversionInchesToCentimeters + " centimeters ");
-    
+        double inches = 300;
+        double conversionInchesToCentimeters = calcFeetAndInchesToCentimeters(inches);
+        System.out.println("  " + inches + " inches is  "
+                + conversionInchesToCentimeters + " centimeters ");
+
     // call
-   
-        double centimeters= calcFeetAndInchesToCentimeters2(6,0);
-        if (centimeters <0.0)
-        {
+        double centimeters = calcFeetAndInchesToCentimeters2(6, 0);
+        if (centimeters < 0.0) {
             System.out.println("Invalid parameter");
         }
         calcFeetAndInchesToCentimeters2(157);
     }
-    
-    
-    public static double calcFeetAndInchesToCentimeters2(double feet, double inches)
-    {
-        if ((feet<0)|| ((inches<0)||(inches>12))){
+
+    public static double calcFeetAndInchesToCentimeters2(double feet, double inches) {
+        if ((feet < 0) || ((inches < 0) || (inches > 12))) {
             System.out.println("Invalid feet or inches parameters");
             return -1;
         }
-        double centimeters = (feet*12)*2.54;
-        centimeters += inches*2.54;
-        System.out.println(feet +" feet "+ inches+ " inches = "+
-                centimeters + " cm");
+        double centimeters = (feet * 12) * 2.54;
+        centimeters += inches * 2.54;
+        System.out.println(feet + " feet " + inches + " inches = "
+                + centimeters + " cm");
         return centimeters;
     }
-    
-    public static double calcFeetAndInchesToCentimeters2(double inches)
-    {if (inches<0)
-    {return -1;
-    
-    }
-    double feet =(int) inches/12;
-    double remainingInches = (int) inches%12;
-    System.out.println(inches +" inches is equal to "+
-            feet + " feet and " +remainingInches + " inches");
-    return calcFeetAndInchesToCentimeters2(feet,remainingInches);
+
+    public static double calcFeetAndInchesToCentimeters2(double inches) {
+        if (inches < 0) {
+            return -1;
+
         }
-    
-    
+        double feet = (int) inches / 12;
+        double remainingInches = (int) inches % 12;
+        System.out.println(inches + " inches is equal to "
+                + feet + " feet and " + remainingInches + " inches");
+        return calcFeetAndInchesToCentimeters2(feet, remainingInches);
+    }
+
     public static double calcFeetAndInchesToCentimeters(double feet,
-                                                         double inches)
-    {
-        if (feet>=0){
-            if ((inches>=0)&& (inches<=12))
-                    {
-                    double totalCentimeters =(((feet*12)+(inches))*2.54);
-                    return totalCentimeters;
-                    
-                    }else {
+            double inches) {
+        if (feet >= 0) {
+            if ((inches >= 0) && (inches <= 12)) {
+                double totalCentimeters = (((feet * 12) + (inches)) * 2.54);
+                return totalCentimeters;
+
+            } else {
                 return -1;
             }
         } else {
             return -1;
         }
-            
+
     }
-    
-    public static double calcFeetAndInchesToCentimeters(double inches)
-    {
-          if (inches>=0)
-          {
-              double feet = (int)inches/12;
-              double remainInches = (int) inches%12;
-              double conversionCentimeters =
-                      calcFeetAndInchesToCentimeters(feet,remainInches);
-              return conversionCentimeters;
-          }else
-          {
-              return -1;
-          }
-          
+
+    public static double calcFeetAndInchesToCentimeters(double inches) {
+        if (inches >= 0) {
+            double feet = (int) inches / 12;
+            double remainInches = (int) inches % 12;
+            double conversionCentimeters
+                    = calcFeetAndInchesToCentimeters(feet, remainInches);
+            return conversionCentimeters;
+        } else {
+            return -1;
+        }
+
     }
-    
-    
-    public static int calculateScore(String playerName,int score)
-    {
-        System.out.println("Player "+ playerName+ " scored "+score + "points ");
-        return score*1000;
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + "points ");
+        return score * 1000;
     }
-    public static int calculateScore(int score)
-    {
-        System.out.println("Unnamed Player scored "+score + " points ");
-        return score*1000;
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed Player scored " + score + " points ");
+        return score * 1000;
     }
- 
-public static int calculateScore()
-    {
+
+    public static int calculateScore() {
         System.out.println("No player name, no player score ");
         return 0;
     }
-
 
 }
