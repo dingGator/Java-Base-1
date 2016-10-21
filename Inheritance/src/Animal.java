e33a9d8cfb47d2e3f03214597afda6cf1a53085d
dingGator <kim@chucrew.net>
2016-10-21 15:52:43 -0400

    Inheritance_Exercise
    Inheritance Exercise from Udemy class

---
+++ Inheritance/src/Animal.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Animal {
+    private String name;
+    private int brain;
+    private int body;
+    private int size;
+    private int weight;
+    public Animal(String name, int brain, int body, int size, int weight) {
+        this.name = name;
+        this.brain = brain;
+        this.body = body;
+        this.size = size;
+        this.weight = weight;
+    }
+    public void eat(){
+        System.out.println(" Animal.eat() called");
+    }
+
+    public void move(){
+    
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getBrain() {
+        return brain;
+    }
+
+    public int getBody() {
+        return body;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+    
+}
---
+++ Inheritance/src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Main {
+    public static void main(String[] args){
+        Animal animal = new Animal ("Animal",1,1,5,5);
+        Dog dog =new Dog("Yorkie",8,20,2,4,1,20,"long silky");
+        dog.eat();
+    }
+}
