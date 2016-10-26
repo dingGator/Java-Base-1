76ea3b4199345bdf6e94f61360a1a6d4f87cde7f
dingGator <kim@chucrew.net>
2016-10-26 10:57:21 -0400

    Encapsulation_Exercise
    Encapsulation Exercise from Udemy class

---
+++ Encapsulation/Encapsulation/src/Printer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted =0;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, 
+           
+            boolean duplex) {
+        if (tonerLevel >= -1 && tonerLevel<=100){
+             
+        this.tonerLevel = tonerLevel;
+        } else { 
+            this.tonerLevel = -1;
+        }
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
+    public int addToner(int tonerAmount){
+        if (tonerLevel > 0 && tonerAmount <=100){
+            if (this.tonerLevel + tonerAmount >100){
+            return -1;
+            }
+            this.tonerLevel +=tonerAmount;
+            return this.tonerLevel;
+        }else {
+            return -1;
+        }
+    }
+    public int printPages(int pages){
+         int pagesToPrint = pages;
+         if (this.duplex){
+             pagesToPrint /=2;
+             System.out.println("printing in duplex mode");
+         }
+         this.pagesPrinted +=pagesToPrint;
+         return pagesToPrint;
+         
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+    
+}
