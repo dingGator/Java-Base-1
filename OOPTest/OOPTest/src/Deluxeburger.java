f308ec1d894b6c624a333a758cd6f5bda1b8faae
dingGator <kim@chucrew.net>
2016-10-29 16:30:44 -0400

    OOPTest_Exercise
    OOPTest Exercise from Udemy class

--- OOPTest/OOPTest/src/Deluxeburger.java
+++ OOPTest/OOPTest/src/Deluxeburger.java
@@ -9,34 +9,36 @@
  * @author Ki
  */
 public class Deluxeburger extends Hamburger {
- public Deluxeburger() {
-        super("Deluxe","Sausage & Bacon", 14.54, "White");
+
+    public Deluxeburger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
         super.addHamburgerAddition1("Chips", 2.75);
         super.addHamburgerAddition2("Drink", 1.81);
-     
-     }
+
+    }
 
     @Override
     public void addHamburgerAddition4(String name, double price) {
-         System.out.println("Cannot add additional items to a deluxe burger"); 
+        System.out.println("Cannot add additional items to a deluxe burger");
         //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void addHamburgerAddition3(String name, double price) {
-         System.out.println("Cannot add additional items to a deluxe burger");
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
+
     @Override
     public void addHamburgerAddition2(String name, double price) {
-      System.out.println("Cannot add additional items to a deluxe burger");
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
 
     @Override
     public void addHamburgerAddition1(String name, double price) {
-      System.out.println("Cannot add additional items to a deluxe burger"); }
- 
-    
+        System.out.println("Cannot add additional items to a deluxe burger");
     }
+
+}
 
     
     
