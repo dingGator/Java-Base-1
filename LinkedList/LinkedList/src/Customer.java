dea008f475d4343792fb4375a979d52009ae21bb
dingGator <kim@chucrew.net>
2016-11-04 16:21:31 -0400

    LinkedList_Exercise
    LinkedList Exercise from Udemy class

---
+++ LinkedList/LinkedList/src/Customer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+    
+}
---
+++ LinkedList/LinkedList/src/Main.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Customer customer = new Customer("Tim", 54.96);
+        Customer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer "+
+                customer.getName()+" is "+customer.getBalance());
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+        for(int i=0; i<intList.size();i++){
+            System.out.println(i+": "+intList.get(i));
+        }
+        
+        System.out.println("************ ");
+        
+        intList.add(1,2);
+          for(int i=0; i<intList.size();i++){
+            System.out.println(i+": "+intList.get(i));
+            
+        }
+    }
+    
+}
