97508e4d744c6e5039d6fbbc30fc152f1ee7f48b
dingGator <kim@chucrew.net>
2016-11-13 16:54:22 -0500

    Packages_Exercise
    Packages Exercise from Udemy class

---
+++ Packages/Packages/src/Main.java
@@ -0,0 +1,43 @@
+
+import javax.xml.soap.Node;
+
+//import javafx.scene.Node;
+//import org.w3c.dom.Node;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ki
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*/ TODO code application logic here
+        Node node = null;*/
+        org.w3c.dom.Node anotherNode = null;
+        
+       //new Main().bleh();
+        
+        
+    }
+    public void bleh(){
+        for (int i =0;i<10;i++){
+            int f = 4;
+            String name = "Bob - ";
+            if (i>7) f++;
+            else{ f +=2;
+             System.out.println(name + f);}
+        }
+       // System.out.println(name + f);
+    */
+                }
+    
+}
