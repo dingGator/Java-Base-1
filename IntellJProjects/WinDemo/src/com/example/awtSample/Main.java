a8f7f1bc3166cf68ed938d7ca6b2091212ab851b
dingGator <kim@chucrew.net>
2016-11-18 10:43:33 -0500

    IntellJProjects_Exercise
    IntellJProjects Exercise from Udemy class

---
+++ IntellJProjects/WinDemo/src/com/example/awtSample/Main.java
@@ -0,0 +1,11 @@
+package com.example.awtSample;
+
+/**
+ * Created by Ki on 11/2/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
