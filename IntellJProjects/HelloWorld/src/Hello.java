8dba139a28e9ff6de3f56388d6c4f953e3b4196d
dingGator <kim@chucrew.net>
2016-11-19 10:23:14 -0500

    Hello_World
    Hello World IJ project from Udemy class

--- IntellJProjects/HelloWorld/src/Hello.java
+++ IntellJProjects/HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@
  * Created by Ki on 10/31/2016.
  */
 public class Hello {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
         System.out.println("Hello World");
     }
 }
