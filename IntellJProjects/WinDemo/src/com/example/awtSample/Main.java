ea9afa93393324285a4c1f2ae740bc18bc49f6c1
dingGator <kim@chucrew.net>
2016-11-19 10:27:14 -0500

    WindoDemo_Exercise_IJ
    WindoDemo Exercise in IJ from Udemy class

--- IntellJProjects/WinDemo/src/com/example/awtSample/Main.java
+++ IntellJProjects/WinDemo/src/com/example/awtSample/Main.java
@@ -4,7 +4,7 @@ package com.example.awtSample;
  * Created by Ki on 11/2/2016.
  */
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         MyWindow myWindow = new MyWindow("Complete Java");
         myWindow.setVisible(true);
     }
