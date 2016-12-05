a490f8cd412c357cf06d910674dccaf9531fca85
dingGator <kim@chucrew.net>
2016-12-05 14:56:11 -0500

    Question8_Exercise
    Question8 Exercise OCA Chpt2

---
+++ IntellJProjects/OCA_SG/SelfTest/SelfTest_Chpt2/src/Question7/Locomotive.java
@@ -0,0 +1,17 @@
+package Question7;
+
+/**
+ * Created by Ki on 12/5/2016.
+ */
+public class Locomotive {
+    Locomotive(){main("hi");}
+
+    public static void main(String[] args) {
+        System.out.print("2 ");
+    }
+
+    public static void main(String args) {
+        System.out.print("3 " + args );
+
+    }
+}
