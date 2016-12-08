90c8f5997c72e8bbdd140d97fa4d52bfa706efde
dingGator <kim@chucrew.net>
2016-12-08 07:51:31 -0500

    Chpt3_Exercise
    Chpt3 Exercise: OCA Chpt3

---
+++ IntellJProjects/OCA_SG/Chpt3/Chpt3/src/Literals/HexTest.java
@@ -0,0 +1,14 @@
+package Literals;
+
+/**
+ * Created by Ki on 12/7/2016.
+ */
+public class HexTest {
+    public static void main(String[] args) {
+        int x = 0X0001;
+        int y = 0x7fffffff;
+        int z= 0xDeadCafe;
+        int z2 = 0XDEADCAFE;
+        System.out.println(" x = " +x + "\n y = " + y + "\n z = " +z+"\n z2 = " +z2 );
+    }
+}
---
+++ IntellJProjects/OCA_SG/Chpt3/Chpt3/src/Literals/Octal.java
@@ -0,0 +1,15 @@
+package Literals;
+
+/**
+ * Created by Ki on 12/7/2016.
+ */
+public class Octal
+{
+    public static void main(String[] args) {
+        int six =06;
+        int seven = 07;
+        int eight = 010;
+        int nine = 011;
+        System.out.println("Octal 010 = "+ eight);
+    }
+}
