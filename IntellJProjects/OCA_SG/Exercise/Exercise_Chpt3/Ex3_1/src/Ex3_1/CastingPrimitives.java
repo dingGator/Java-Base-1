b71850d9d6a054c546a63d8a4eba1181b192cbc0
dingGator <kim@chucrew.net>
2016-12-07 20:16:31 -0500

    Chpt3_Exercise
    Chpt3 Exercise OCA Chpt3

---
+++ IntellJProjects/OCA_SG/Exercise/Exercise_Chpt3/Ex3_1/src/Ex3_1/CastingPrimitives.java
@@ -0,0 +1,15 @@
+package Ex3_1;
+
+/**
+ * Created by Ki on 12/7/2016.
+ */
+public class CastingPrimitives {
+
+    float f = 234.56f;
+    short s = (short)f;
+    static float f2 = 234.56f;
+    static short s2 = (short)f2;
+    public static void main(String[] args) {
+        System.out.println("Casting short s = (short) f  "+  s2);
+    }
+}
