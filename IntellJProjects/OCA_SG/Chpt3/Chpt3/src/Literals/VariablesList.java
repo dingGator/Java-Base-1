d07581cba152443f6ebeab9768434a0a50aa6ad0
dingGator <kim@chucrew.net>
2016-12-11 20:17:50 -0500

    Literals_Exercise
    Literals Exercise OCA Chpt3

---
+++ IntellJProjects/OCA_SG/Chpt3/Chpt3/src/Literals/VariablesList.java
@@ -0,0 +1,26 @@
+package Literals;
+
+/**
+ * Created by Ki on 12/11/2016.
+ */
+public class VariablesList {
+    static int a, b, c;
+    static int a1 = 1, b1 = 2, c1 = 3;
+    static int j, k = 1, l, m = 3 + k;
+
+    public static void main(String[] args) {
+        System.out.println("\n a = " + a +
+                "\n b = " + b +
+                "\n c = " + c +
+                "\n a1 = " + a1 +
+                "\n b1 = " + b1 +
+                "\n c1 = " + c1 +
+                "\n j1 = " + j +
+                "\n k =" + k +
+                "\n l= " + l +
+                "\n m= " + m
+        );
+    }
+
+
+}
