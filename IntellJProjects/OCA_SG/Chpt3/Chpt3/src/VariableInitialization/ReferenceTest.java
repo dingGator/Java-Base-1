dc1bb58ba4362b42819b15b4f5ee59aec6ab79be
dingGator <kim@chucrew.net>
2016-12-14 12:39:43 -0500

    VariablesInitialization_Exercise
    VariablesInitialization Exercise OCA Chpt3

--- IntellJProjects/OCA_SG/Chpt3/Chpt3/src/VariableInitialization/ReferenceTest.java
+++ IntellJProjects/OCA_SG/Chpt3/Chpt3/src/VariableInitialization/ReferenceTest.java
@@ -3,17 +3,23 @@ package VariableInitialization;
 /**
  * Created by Ki on 12/13/2016.
  */
+
 import java.awt.Dimension;
+
 public class ReferenceTest {
     public static void main(String[] args) {
-        Dimension a1 = new Dimension(5,10);
+        Dimension a1 = new Dimension(5, 10);
         System.out.println("a1.height = " + a1.height);
-        Dimension b1= a1;
-        b1.height= 30;
-        System.out.println("a1.height = " + a1.height+ " after change to b1");
-        //both a1 and b1 point to same instance of Dimension object
+        Dimension b1 = a1;
+        System.out.println("b1.height = " + b1.height);
+
+        b1.height = 30;
+        System.out.println("a1.height = " + a1.height +
+                "\n  after change to b1:  b1.height= 30;");
+        //both a1 and b1 point to same INSTANCE of Dimension object
         // thus when b1 is changed to 30 then a1 is changed to 30 too WOW!!!
 
+        System.out.println("b1.height = " + b1.height + " after change to b1");
 
 
     }
