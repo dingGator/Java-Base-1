7226be603b49aa13430235a1b9f52a3d6ab447bc
dingGator <kim@chucrew.net>
2017-01-12 22:10:32 -0500

    Chpt5Exercise
    Chpt5 Exercise

--- IntellJProjects/OCA_SG/Chpt5/src/Array/ArrayReference.java
+++ IntellJProjects/OCA_SG/Chpt5/src/Array/ArrayReference.java
@@ -9,8 +9,14 @@ public class ArrayReference {
 
     int[] blots;
     int[][] squeegees = new int[3][];
-    //blots = squeegees;  //  bcs squeegess is a 2-D  array of int
+    //blots = squeegees;  //  NO  bcs squeegess is a 2-D  array of int
     int []blocks =  new int[6];
     blots = blocks;   // bcs blocks is an int array
-        
+        int [][] books = new int [3][];
+        int [] numbers = new int[6];
+        int aNumber =7;
+        //books[0] = aNumber;  // NO expecting an int array not an int
+        books[0] = numbers;  //OK numbers is an int array
+
+
     }}
