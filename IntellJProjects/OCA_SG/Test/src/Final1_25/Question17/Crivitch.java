cbc5f7104e4d7e1c5c85ddc385645ebb59404264
dingGator <kim@chucrew.net>
2017-01-25 21:19:08 -0500

    PracticeQuiz_1_25
    Practice Quiz 1/25

--- IntellJProjects/OCA_SG/Test/src/Final1_25/Question17/Crivitch.java
+++ IntellJProjects/OCA_SG/Test/src/Final1_25/Question17/Crivitch.java
@@ -6,11 +6,11 @@ package Final1_25.Question17;
 public class Crivitch {
     public static void main(String[] args) {
         int x =0;
-        //int y =x;
-        int y =10;
-        //int y =11;
-        //int y = 12;
-        //int y = 13;
+        //int y =x;     //output 1
+        //int y =10;    //output 11
+        int y =11;      //output 12
+        //int y = 12;   //output13
+        //int y = 13;   //output14
         do{} while(x++ <y);
         System.out.println(x);
     }
