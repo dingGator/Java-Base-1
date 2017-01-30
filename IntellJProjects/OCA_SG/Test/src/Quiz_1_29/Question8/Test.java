933b4abbbb9bb8ad95138ad4e5f6604a0ec8cb87
dingGator <kim@chucrew.net>
2017-01-29 23:34:48 -0500

    Quiz_1_29
    Quiz 1/29

---
+++ IntellJProjects/OCA_SG/Test/src/Quiz_1_29/Question8/Test.java
@@ -0,0 +1,20 @@
+package Quiz_1_29.Question8;
+
+/**
+ * Created by Ki on 1/29/2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        short [][]b = new short[4][4];
+        short [][]big = new short[2][3];
+        short b3 = 8;
+        short b2 [][][][]= new short[2][3][2][2];
+        //b2[1][1] =big;                //compile
+        //b[1][0] = b3;                 //compile
+        //b2[0][1][1] = b;              //  not compile
+        //b2[0][2][1] = b[1][0];        // not compile
+        //b2[1][1][0][1]= b[1][0];      //compile
+        //b2[1][1]=b;                   // compile
+
+    }
+}
