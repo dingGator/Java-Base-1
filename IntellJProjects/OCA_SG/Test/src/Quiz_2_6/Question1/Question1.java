b72d567bbab42429852712039d9c26bb850021c3
dingGator <kim@chucrew.net>
2017-02-06 23:05:35 -0500

    Quiz_2_6
    Quiz 2/6

---
+++ IntellJProjects/OCA_SG/Test/src/Quiz_2_6/Question1/Question1.java
@@ -0,0 +1,17 @@
+package Quiz_2_6.Question1;
+
+/**
+ * Created by Ki on 2/6/2017.
+ */
+public class Question1 {
+    public static void main(String[] args) {
+
+
+        String d = "aiko ";
+        d += d;
+        d += "ahnay ";
+        d.concat("shockamal ");
+        d.toUpperCase();
+        System.out.println(d);
+    }
+}
