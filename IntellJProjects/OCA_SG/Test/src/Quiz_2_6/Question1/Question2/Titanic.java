dcabb27ebeaf45da8b04b7c0b653be972f65ab68
dingGator <kim@chucrew.net>
2017-02-06 23:11:47 -0500

    Quiz_2_6
    Quiz 2/6

---
+++ IntellJProjects/OCA_SG/Test/src/Quiz_2_6/Question1/Question2/Titanic.java
@@ -0,0 +1,16 @@
+package Quiz_2_6.Question1.Question2;
+
+/**
+ * Created by Ki on 2/6/2017.
+ */
+public class Titanic {
+    public static void main(String[] args) {
+        Boolean b1 =true;
+        boolean b2 = false;
+        boolean b3 = true;
+        if((b1&b2)|(b2&b3)&b3)
+            System.out.println("alpha ");
+        if((b1 = false)| (b1&b3) | (b1|b2))
+            System.out.println("beta ");
+    }
+}
