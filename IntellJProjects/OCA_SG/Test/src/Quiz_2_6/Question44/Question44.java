13f589349666a0d8eb7061680b741e25e3b54dd5
dingGator <kim@chucrew.net>
2017-02-10 23:03:58 -0500

    Quiz_2_6
    Quiz 2/6

---
+++ IntellJProjects/OCA_SG/Test/src/Quiz_2_6/Question44/Question44.java
@@ -0,0 +1,15 @@
+package Quiz_2_6.Question44;
+
+/**
+ * Created by Ki on 2/10/2017.
+ */
+public class Question44 {
+    public static void main(String[] args) {
+        int x=2;
+        for(int y  =2; y>0; y--){
+            System.out.println(x + " "+ y + " ");
+            x++;
+        }
+    //    System.out.println((x + " "+ y));
+    }
+}
